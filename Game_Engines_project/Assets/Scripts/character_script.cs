467f768e72b5c950d6069a8b593043f9849b1876
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-17 21:37:05 +0000

    added a jump
    allow the player to not only jump but also use gravity so that they continuosly fly away when pressing the jump button

--- Game_Engines_project/Assets/Scripts/character_script.cs
+++ Game_Engines_project/Assets/Scripts/character_script.cs
@@ -6,6 +6,8 @@ public class character_script : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float roatateSpeed = 30f;
+    public float jumpHeight = 1.0F;
+    public float gravity = 20.0f;
     public Camera playerCamera;
     CharacterController characterController;
     Vector3 direction = Vector3.zero;
@@ -21,14 +23,26 @@ public class character_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        direction = transform.TransformDirection(direction);
-        direction *= walkingSpeed;
+        if (characterController.isGrounded)
+        {
 
+
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            direction = transform.TransformDirection(direction);
+            direction *= walkingSpeed;
+
+            characterController.Move(direction * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Q))
+                transform.Rotate(-Vector3.up * roatateSpeed * Time.deltaTime);
+            else if (Input.GetKey(KeyCode.E))
+                transform.Rotate(Vector3.up * roatateSpeed * Time.deltaTime);
+            if (Input.GetButton("Jump"))
+                direction.y = jumpHeight;
+        }
+
+        direction.y -= gravity * Time.deltaTime;
         characterController.Move(direction * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Q))
-            transform.Rotate(-Vector3.up * roatateSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.E))
-            transform.Rotate(Vector3.up * roatateSpeed * Time.deltaTime);
+
+
     }
 }
