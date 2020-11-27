0b4db183fea3c5e173b8eb80a00f5761bf8e41f0
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-27 16:58:52 +0000

    bug fixing and weapon spawning

--- Game_Engines_project/Assets/Scripts/character_script.cs
+++ Game_Engines_project/Assets/Scripts/character_script.cs
@@ -15,7 +15,15 @@ public class character_script : MonoBehaviour
     public float RotateSpeed = 30f;
     CharacterController characterController;
     Vector3 direction = Vector3.zero;
-    
+    public static int ammo = 5;
+    public float bullets;
+    public GameObject bullet;
+    public GameObject barrel;
+    public Transform hand;
+    public GameObject gun;
+    public bool canshoot = true;
+    public float rateoffire;
+
     [HideInInspector]
     public bool canmove = true;
     // Start is called before the first frame update
@@ -44,14 +52,46 @@ public class character_script : MonoBehaviour
         direction.y -= gravity * Time.deltaTime;
         characterController.Move(direction * Time.deltaTime);
 
+        if (Input.GetMouseButtonDown(0) && ammo >= 1 && canshoot == true)
+        {
+          //  shoot();
+
+        }
         // Player and Camera rotation
-          if (canmove)
+        if (canmove)
          {
              rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
              rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
          }
+    
+        
+        
+     
+    }
+    void shoot()
+    {
+        Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+        ammo -= 1;
+        canshoot = false;
 
+        StartCoroutine(FireRate());
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "gun")
+        {
+            Destroy(other.gameObject);
+        GameObject gunnner =    Instantiate<GameObject>(gun, hand.position, hand.rotation);
+            gunnner.transform.parent = hand.transform;
+           
+        }  
+    }
+    IEnumerator FireRate()
+    {
+        yield return new WaitForSeconds(rateoffire);
+        canshoot = true;
     }
 }
---
+++ Game_Engines_project/Assets/Scripts/move_bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_bullet : MonoBehaviour
+{
+    public float speed;
+    public float damage = 1f;
+  
+
+
+    void Update()
+    {
+        transform.position += (transform.forward * Time.deltaTime * speed);
+    }
+   
+    void OnCollisionEnter(Collision other)
+    {
+       
+
+        if (other.gameObject.tag == "enemy")
+        {
+            //  other.gameObject.GetComponent<Rigidbody>
+          //  other.gameObject.GetComponent<enemy>().Health = -damage;
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "building")
+        {
+         
+            Destroy(this.gameObject);
+          
+        }
+
+        if (other.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (speed == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
